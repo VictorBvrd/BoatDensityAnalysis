b00ca40748da1d71035c3029d5e44a2cbbdb99e7
VictorBvrd <110540974+VictorBvrd@users.noreply.github.com>
2022-08-04 15:49:16 +0300

    Update BoatDensityAnalysis.py

--- BoatDensityAnalysis.py
+++ BoatDensityAnalysis.py
@@ -3,5 +3,5 @@ import numpy as np
 import os
 
 df=pd.DataFrame()
-df=pd.read_csv('C:/Users/Archi Office PC/Desktop/Victor Bouvard/Program/ARCHIPELAGO_AEGEAN_2017_2021-001.csv')
+df=pd.read_csv('ARCHIPELAGO_AEGEAN_2017_2021-001.csv')
 display(df)
